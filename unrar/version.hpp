b8bcc3695ecf7630076c6ce2c4f8d986b4f11473
Force Charlie <charlieio@outlook.com>
2023-07-22 23:07:05 +0800

    bump 6.23.1

--- unrar/version.hpp
+++ unrar/version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     6
-#define RARVER_MINOR    21
-#define RARVER_BETA      0
-#define RARVER_DAY      16
-#define RARVER_MONTH     2
+#define RARVER_MINOR    23
+#define RARVER_BETA      1
+#define RARVER_DAY      19
+#define RARVER_MONTH     7
 #define RARVER_YEAR   2023
